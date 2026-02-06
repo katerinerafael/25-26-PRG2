784f021079b4af2dbe5228ba717d5e2515505cea
Katerine Lisbeth Rafael Bourdierd <katerine.rafael@unealum.local>
2026-02-06 11:12:54 +0100

    Limpiador de pantalla

--- entregas/rafaelKaterine/src/JEdlin.java
+++ entregas/rafaelKaterine/src/JEdlin.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class JEdlin {
+    private String[] lines;
+    private int activateLine;
+    private Scanner scanner;
+
+    public JEdlin() {
+        lines = new String[10];
+        Arrays.fill(lines,"");
+        lines[0] = "Bienvenidos al editor EDLIN";
+        lines[1]= "Utilice el menu inferior para editar el texto";
+        lines[2]= "---------------------------------------------";
+        activateLine = 0;
+        scanner = new Scanner(System.in)
+    }
+
+    private void clearScreen(){
+        System.out.print("/////////");
+        System.out.flush();
+        for (int i = 0; i < 50; i++) System.out.println();
+    }
 
     
 }
+
+
