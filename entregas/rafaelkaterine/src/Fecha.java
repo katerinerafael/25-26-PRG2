4acb884cc2c4db6805a5f3c03722f0058a7e94ea
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-02-27 10:21:01 +0000

    feat: agregar método tiene30Dias

--- entregas/rafaelkaterine/src/Fecha.java
+++ entregas/rafaelkaterine/src/Fecha.java
@@ -5,6 +5,7 @@ class Fecha {
     public Fecha()
     public Fecha(Fecha fecha)
 
+    
     public void editar(int dia, int mes, int año)
     public void mostrar()
 
@@ -12,6 +13,7 @@ class Fecha {
     public boolean esPosterior(int dia, int mes, int año)
     public boolean finDeSemana()
     public boolean esBisiesto()
+    public boolean tiene30Dias()
 
     public Fecha[] calendarizar(Fecha fecha, int frecuenciaEnDias)    
     public Fecha[] calendarizar(Fecha fecha, int frecuencia, int tipoDeCalendario)    
