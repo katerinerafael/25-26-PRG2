104de190e5cccb022ab61c50838d884bd27e94e6
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-03-13 10:41:51 +0000

    refactor: Simplificar métodos de cálculo de límites y mejorar la legibilidad del código

--- entregas/rafaelkaterine/src/Intervalo.java
+++ entregas/rafaelkaterine/src/Intervalo.java
@@ -4,81 +4,69 @@ class Intervalo{
      private double puntoMedio;
      private double longitud;
 
-     public Intervalo (double inferior, double superior) {
+
+    private double getInferior() {
+        return puntoMedio - longitud / 2;
+    }
+    private double getSuperior() {
+        return puntoMedio + longitud / 2;
+    }
+
+
+    public Intervalo (double inferior, double superior) {
         assert inferior <= superior;
         this.longitud = superior - inferior;
         this.puntoMedio = inferior + this.longitud / 2;
     }
-
     public Intervalo (double superior) {
         this(0, superior);
     }
-
     public Intervalo () {
         this(0, 0);
-    }
-    
+    }  
     public Intervalo (Intervalo intervalo) {
         this(intervalo.puntoMedio, intervalo.longitud);
     }
 
-    public Intervalo clone() {
-        return new Intervalo(this);
-    }
 
-    public double longitud() {
-        return this.longitud;
+    public boolean incluye(double punto) {
+        return this.getInferior() <= punto && this.getSuperior() >= punto;
     }
-
+    public boolean incluye(Intervalo intervalo) {
+        assert intervalo != null;
+        return this.incluye(intervalo.getInferior()) && this.incluye(intervalo.getSuperior());
+    }
+    public boolean equals(Intervalo intervalo) {
+        return this.puntoMedio == intervalo.puntoMedio && this.longitud == intervalo.longitud;
+    }
+    public void mostrar() {
+        System.out.println("[" + this.getInferior() + ", " + this.getSuperior() + "]");
+    }
+    public void oponer() {
+        this.puntoMedio = -this.puntoMedio;
+    }
+    public void doblar() {
+        this.longitud *= 2;
+    }
     public void desplazar(double desplazamiento) {
         this.puntoMedio = this.puntoMedio + desplazamiento;
     }
-
     public Intervalo desplazado(double desplazamiento) {
         Intervalo intervalo = this.clone();
         intervalo.desplazar(desplazamiento);
         return intervalo;
     }
-
-    public boolean incluye(double punto) {
-        double inferior = this.puntoMedio - this.longitud / 2;
-        double superior = this.puntoMedio + this.longitud / 2;
-        
-        return punto >= inferior && punto <= superior;
+    public Intervalo clone() {
+        return new Intervalo(this);
     }
-
-    public boolean incluye(Intervalo intervalo) {
-        assert intervalo != null;
-        double inferior = this.puntoMedio - this.longitud / 2;
-        double superior = this.puntoMedio + this.longitud / 2;
-        double otroInferior = intervalo.puntoMedio - intervalo.longitud / 2; 
-        double otroSuperior = intervalo.puntoMedio + intervalo.longitud / 2;
-
-        return inferior <= otroInferior && superior >= otroSuperior;
+    public double longitud() {
+        return this.longitud;
     }
-
-    public boolean equals(Intervalo intervalo) {
-        return this.puntoMedio == intervalo.puntoMedio && this.longitud == intervalo.longitud;
-    }
-
-    public void oponer() {
-        this.puntoMedio = -this.puntoMedio;
-    }
-
-    public void doblar() {
-        this.longitud *= 2;
-    }
-
-
-    public void mostrar() {
-        double inferior = this.puntoMedio - this.longitud / 2;
-        double superior = this.puntoMedio + this.longitud / 2;
-        System.out.println("[" + inferior + ", " + superior + "]");
-    }
-
     public double puntoMedio() {
         return this.puntoMedio;
-    };
+    }
+
+
 
     public Intervalo[] trocear(int numeroTrozos) {
         Intervalo[] trozos = new Intervalo[numeroTrozos];
@@ -97,12 +85,11 @@ class Intervalo{
         
         System.out.print("Intervalo original: ");
         intervalo.mostrar(); 
-        
         System.out.println("Longitud: " + intervalo.longitud());
-        
-        intervalo.desplazar(5);
-        System.out.print("Tras desplazar 5 unidades: ");
+        intervalo.doblar();
+        System.out.print("Tras doblar: ");
         intervalo.mostrar(); 
+        System.out.println("Longitud: " + intervalo.longitud());
     }
 
 /*
