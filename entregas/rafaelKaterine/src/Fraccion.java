a2761292ec2fa4a835381aca933b05cb9ff7f2ef
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-03-17 12:12:27 +0000

    feat: creacion de un constructor

--- entregas/rafaelKaterine/src/Fraccion.java
+++ entregas/rafaelKaterine/src/Fraccion.java
@@ -2,6 +2,9 @@ public class Fraccion {
     private double numerador;
     private double denominador;
 
-
+    public Fraccion(double numerador, double denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
 
 }
