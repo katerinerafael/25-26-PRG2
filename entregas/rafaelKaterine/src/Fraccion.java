c4f73dcda14187392de7dc45097498446ebcb63c
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-03-17 12:23:51 +0000

    feat: creacion de diferentes constructores y el metodo clonar

--- entregas/rafaelKaterine/src/Fraccion.java
+++ entregas/rafaelKaterine/src/Fraccion.java
@@ -1,10 +1,17 @@
 public class Fraccion {
-    private double numerador;
-    private double denominador;
+    private int numerador;
+    private int denominador;
 
-    public Fraccion(double numerador, double denominador) {
+    public Fraccion(int numerador, int denominador) {
         this.numerador = numerador;
         this.denominador = denominador;
     }
+    public Fraccion(Fraccion fraccion) {}
+    public Fraccion(int numerador) {
+        this.numerador = numerador;
+        this.denominador = 1;
+    }
+    public Fraccion(){}
 
+    public Fraccion clonar(){}
 }
