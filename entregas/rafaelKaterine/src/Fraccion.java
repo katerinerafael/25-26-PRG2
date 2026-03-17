670f296966f8ec54bc3de41c5d3d7c483824b25b
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-03-17 13:02:29 +0000

    feat: Creacion completa de Vista Publica

--- entregas/rafaelKaterine/src/Fraccion.java
+++ entregas/rafaelKaterine/src/Fraccion.java
@@ -2,17 +2,11 @@ public class Fraccion {
     private int numerador;
     private int denominador;
 
-    public Fraccion(int numerador, int denominador) {
-        this.numerador = numerador;
-        this.denominador = denominador;
-    }
+    public Fraccion(int numerador, int denominador) {}
     public Fraccion(Fraccion fraccion) {}
-    public Fraccion(int numerador) {
-        this.numerador = numerador;
-        this.denominador = 1;
-    }
+    public Fraccion(int numerador) {}
     public Fraccion(){}
-    
+
     public Fraccion clonar(){}
     public Fraccion valorAbsoluto() {}
 
