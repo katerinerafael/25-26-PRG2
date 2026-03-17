2a0ece5a5df055d98732871159814c23b25793f1
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-03-17 13:55:35 +0000

    feat: arreglos menores

--- entregas/rafaelKaterine/src/Cliente.java
+++ entregas/rafaelKaterine/src/Cliente.java
@@ -3,7 +3,7 @@ public class Cliente {
     public static void main(String[] args) {
         Fraccion fraccion = new Fraccion(1, 2);
         Fraccion fraccion2 = new Fraccion(3, 4);
-        fraccion.sumar(fraccion2).mostrar();
-
+        fraccion.sumar(fraccion2);
+        System.out.println(fraccion);    
     }
 }
