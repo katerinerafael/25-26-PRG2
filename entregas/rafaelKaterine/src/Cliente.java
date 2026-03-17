ac806352634ae3bade6f762b911a88a52d5251cc
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-03-17 13:29:31 +0000

    feat: Creacion del main del cliente de la clase

---
+++ entregas/rafaelKaterine/src/Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente {
+
+    public static void main(String[] args) {
+        Fraccion fraccion = new Fraccion(1, 2);
+        Fraccion fraccion2 = new Fraccion(3, 4);
+        fraccion.sumar(fraccion2).mostrar();
+
+    }
+}
