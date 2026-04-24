e5522f2027710cf5848f534bd297803bce3a2ca8
Kath <katerine.rafael@alumnos.uneatlantico.es>
2026-04-24 08:51:05 +0000

    feat: cambio de nombre de variable

--- entregas/rafaelKaterine/src/Carrera.java
+++ entregas/rafaelKaterine/src/Carrera.java
@@ -2,6 +2,6 @@ class Carrera {
     public static void main(String[] args) {
 
         CarreraDeCaballos kentucky = new CarreraDeCaballos();
-        kentucky.simular();
+        kentucky.empezar();
     }
 }
